ce08937174726efcfc096f440239698438676469
elcumanesito <danielgonzalez101998@gmail.com>
2023-12-11 19:33:56 -0400

    examennivel4

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+            return Usuario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+
+     public function store(Request $request){$usuario = new Usuario();
+     $usuario->nombres = $request->nombres;
+     $usuario->save();
+        return "El usuario se guardo correctamente";}
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id){$usuario = Usuario::find($id);$usuario->nombres = $request->nombres;
+    $usuario->save();
+        return "Actualizado correctamente";}
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id){$usuario = Usuario::find($id);$usuario->delete();
+        return "Eliminado Correctamente";}
+
+}
